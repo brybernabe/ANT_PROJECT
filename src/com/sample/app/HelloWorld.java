3a60d788145170bd270b5ade2e367cc65fe3b2c9
brybernabe <bryanandrewb@gmail.com>
2018-10-17 17:56:29 +0800

    Update HelloWorld.java

--- src/com/sample/app/HelloWorld.java
+++ src/com/sample/app/HelloWorld.java
@@ -1,12 +1,11 @@
 package com.sample.app;
 
 public class HelloWorld {
-    String password = "pass";
-    public static boolean fo(String x){
+        public static boolean fo(String x){
         System.out.print("asdasd");
         return x.equals("asd");
     }
     public static void main(String[] as){
-        password = "asd";
+        String password = "asd";
     }
 }
